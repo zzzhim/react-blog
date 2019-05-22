d270108f06555ad1e1e6e7028b45b8e600293311
zzzhim <3218566389@qq.com>
2019-05-22 16:25:06 +0800

    admin----获取文章列表

--- admin/src/views/list/index.js
+++ admin/src/views/list/index.js
@@ -1,10 +1,170 @@
-import React, { PureComponent } from 'react'
+/* eslint-disable no-script-url */
+import React, { PureComponent, Fragment } from 'react'
+import { Table, Divider, Tag, Button } from 'antd'
 import style from './style.module.scss'
+import { request } from '../../utils'
 
 class List extends PureComponent {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    componentDidMount() {
+        request({
+            url: '/get_article_list',
+            params: {
+                current: 1,
+                pageSize: 10
+            }
+        }).then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
     render() {
+        const columns = [
+            {
+                title: 'id',
+                dataIndex: 'key',
+                key: 'key',
+                align: 'center'
+            },
+            {
+                title: '标题',
+                dataIndex: 'title',
+                key: 'title',
+                align: 'center',
+                // eslint-disable-next-line jsx-a11y/anchor-is-valid
+                render: title => <a href="javascript:;">{title}</a>,
+            },
+            {
+                title: '简介',
+                dataIndex: 'introduction',
+                align: 'center',
+                key: 'introduction',
+            },
+            {
+                title: '标签',
+                key: 'tags',
+                dataIndex: 'tags',
+                align: 'center',
+                render: tags => (
+                    <span>
+                        {
+                            tags.map((tag) => {
+                                return (
+                                    <Tag color={ '#1890ff' } key={ tag }>
+                                        {tag.toUpperCase()}
+                                    </Tag>
+                                )
+                            })
+                        }
+                    </span>
+                ),
+            },
+            {
+                title: '创建时间',
+                dataIndex: 'create_time',
+                align: 'center',
+                key: 'create_time',
+                sorter: (a, b) => a.age - b.age
+            },
+            {
+                title: '最近更新',
+                dataIndex: 'update_time',
+                align: 'center',
+                key: 'update_time',
+                sorter: (a, b) => a.age - b.age
+            },
+            {
+                title: '发布',
+                dataIndex: 'is_show',
+                align: 'center',
+                key: 'is_show',
+                render: is_show => (
+                    <span>
+                        { is_show ? '已发布' : '未发布' }
+                    </span>
+                )
+            },
+            {
+                title: '操作',
+                align: 'center',
+                key: 'action',
+                render: (text, record) => (
+                    <span>
+                        <Button type="primary">发布</Button>
+                        <Divider type="vertical" />
+                        <Button type="primary">编辑</Button>
+                        <Divider type="vertical" />
+                        <Button type="danger">删除</Button>
+                    </span>
+                ),
+            },
+        ]
+
+        const data = [
+            {
+                key: '1',
+                title: 'John Brown',
+                introduction: 'introduction',
+                address: 'New York No. 1 Lake Park',
+                tags: ['nice', 'developer'],
+                create_time: '2019-05-22 14:30:48',
+                update_time: '2019-05-22 14:31:05',
+                is_show: 0
+            },
+            {
+                key: '2',
+                title: 'John Brown',
+                introduction: 'introduction',
+                address: 'New York No. 1 Lake Park',
+                tags: ['nice', 'developer'],
+                create_time: '2019-05-22 14:30:48',
+                update_time: '2019-05-22 14:31:04',
+                is_show: 0
+            },
+            {
+                key: '3',
+                title: 'John Brown',
+                introduction: 'introduction',
+                address: 'New York No. 1 Lake Park',
+                tags: ['nice', 'developer'],
+                create_time: '2019-05-22 14:30:48',
+                update_time: '2019-05-22 14:31:09',
+                is_show: 0
+            },
+        ]
+
+        const pagination = {
+            position: 'bottom'
+        }
+
+        const rowSelection = {
+            onChange: (selectedRowKeys, selectedRows) => {
+              console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+            },
+            getCheckboxProps: record => ({
+              disabled: record.name === 'Disabled User', // Column configuration not to be checked
+              name: record.name,
+            }),
+        };
+
         return (
-            <div className={ style.color }>home</div>
+            <Fragment>
+                <div className={style.container}>
+                    <div className={style.table}>
+                        <Table
+                            rowSelection={rowSelection}
+                            columns={columns}
+                            dataSource={data}
+                            pagination={pagination}/>
+                    </div>
+                </div>
+            </Fragment>
         )
     }
 }
